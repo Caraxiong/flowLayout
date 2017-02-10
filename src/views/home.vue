6d3847a8f65a71015350a35fb308122832262039
xiongmeijuan <hzxmj@tairanchina.com>
2017-02-10 15:32:18 +0800

    add aly animation

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,13 @@
 <template>
 	<div class="ali-main-market">
 		<div class="home-p">
-			<a href="javascript:void(0)" class="home-li" v-for = "item in imgList">
-				<span :data-imag="item.img" :data-ie8img="item.ie8img" :style="{'background-image':'url('+item.img+')'}" :class="{hover:isHover}"></span>
+			<a href="javascript:void(0)" class="home-li" v-for = "item in imgList" @mouseover="trans($event ,1)" @mouseout="trans($event ,-1)">
+				<span :class="{hover:isHover}" :style="{'background-image':'url('+item.img+')'}"></span>
 			</a>
+		</div>
+		<div>
+			<div id="test" class="progress">0%</div>
+			<input type="button" value="Run" @click="scrollBar"/>
 		</div>
 	</div>
 </template>
@@ -14,8 +18,7 @@
 		data: function(){
 			return {
 				imgList: [],
-				isHover:false,
-				indexBanner_isIE9: false
+				isHover:false
 			}
 		},
 		mounted: function(){
@@ -24,11 +27,59 @@
 			})
 		},
 		methods: {
-			getMsg: function(){
+			getMsg: function() {
 				axios.get('src/data/home.json')
 					.then((res) => {
 						this.imgList = res.data
 					})
+			},
+			scrollBar: function() {
+				window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+				var ele = document.getElementById("test");
+				var progress = 0;
+
+				function step(timestamp) {
+				    progress += 1;
+				    ele.style.width = progress + "%";
+				    ele.innerHTML=progress + "%";
+				    if (progress < 100) {
+				        requestAnimationFrame(step);
+				    }
+				}
+				requestAnimationFrame(step);
+				// document.getElementById("run").addEventListener("click", function() {
+				//     ele.style.width = "1px";
+				//     progress = 0;
+				//     requestAnimationFrame(step);
+				// }, false);
+			},
+			trans: function(event, num){
+				window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+				var bgY = 0;
+				var distance = 75;
+				var multiple = 60;
+				var timeFactor;
+
+				function step(timestamp) {
+			    	event.target.style.backgroundPositionY = -(bgY * distance) + 'px'
+				}
+				function stepAdd(timestamp) {
+					bgY += 1;
+					bgY < multiple ? (step(),timeFactor = requestAnimationFrame(stepAdd)) : cancelAnimationFrame(timeFactor);
+				}
+				function stepMinus(timestamp) {
+					bgY -= 1;
+					bgY > 0 ? (step(),timeFactor = requestAnimationFrame(stepMinus)) : cancelAnimationFrame(timeFactor);
+
+				}
+				cancelAnimationFrame(timeFactor);
+				if(num > 0){
+					requestAnimationFrame(stepAdd);
+				}else{
+					requestAnimationFrame(stepMinus);
+				}
+				
 			}
 		}
 	}
